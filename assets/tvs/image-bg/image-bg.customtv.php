fdb68f8315cff0687d6336123929ad38300354db
liber87 <bd87@bk.ru>
2021-07-03 19:06:29 +0300

    Update image-bg.customtv.php

--- assets/tvs/image-bg/image-bg.customtv.php
+++ assets/tvs/image-bg/image-bg.customtv.php
@@ -1,17 +1,20 @@
 <style>
+	.image-bg{position:relative;}
+	.image-bg img{max-height:100%;}
 	.mimipic_thumb{position:absolute; top:0;left:0;width:30px;height:30px; padding:2px;z-index:22;cursor:pointer;}
-	.minipic_show{width:128px; height:128px; position:absolute;top:30px; background:white; padding:5px; z-index:99;    box-shadow: 0 0.2rem 2rem rgb(0 0 0 / 10%); display:none;}
+	.minipic_show{width:128px; height:128px; position:absolute;top:-128px; background:white; padding:5px; z-index:99;    box-shadow: 0 0.2rem 2rem rgb(0 0 0 / 10%); display:none;}
 	.mimipic_thumb:not(:empty):hover + .minipic_show {display:block;}	
 </style>
-<div class="mimipic_thumb" id="minipic_thumb_<?=$field_id?>">
-	<?php if ($field_value!='') echo '<img src="./../'.$modx->runSnippet('phpthumb',array('input'=>$field_value,'options'=>'w=30,h=30,far=c,bg=FFFFFF')).'">';?>
+<div class="image-bg">
+	<div class="mimipic_thumb" id="minipic_thumb_<?=$field_id?>">
+		<?php if ($field_value!='') echo '<img src="./../'.$modx->runSnippet('phpthumb',array('input'=>$field_value,'options'=>'w=30,h=30,far=c,bg=FFFFFF')).'">';?>
+	</div>
+	<div class="minipic_show" id="minipic_show_<?=$field_id?>">
+		<?php if ($field_value!='') echo '<img src="./../'.$modx->runSnippet('phpthumb',array('input'=>$field_value,'options'=>'w=128,h=128,far=c,bg=FFFFFF')).'">';?>
+	</div>
+	<input type="text" id="tv<?=$field_id?>" class="form-control" name="tv<?=$field_id?>" value="<?=$field_value?>" onchange="documentDirty=true;getminipic(this.value,<?=$field_id?>);" style="padding-left:36px;">
+	<input class="form-control" type="button" value="Вставить" onclick="BrowseServer('tv<?=$field_id?>')">
 </div>
-<div class="minipic_show" id="minipic_show_<?=$field_id?>">
-	<?php if ($field_value!='') echo '<img src="./../'.$modx->runSnippet('phpthumb',array('input'=>$field_value,'options'=>'w=128,h=128,far=c,bg=FFFFFF')).'">';?>
-</div>
-
-<input type="text" id="tv<?=$field_id?>" class="form-control" name="tv<?=$field_id?>" value="<?=$field_value?>" onchange="documentDirty=true;getminipic(this.value,<?=$field_id?>);" style="padding-left:36px;">
-<input class="form-control" type="button" value="Вставить" onclick="BrowseServer('tv<?=$field_id?>')" style="position:absolute;top:0;right:5px;">
 <script>
 	if (typeof getminipic != 'function') {
 		function getminipic(pic,id){
